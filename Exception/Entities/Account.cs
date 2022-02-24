74567ffe97eab8b613dc803c827281fc9e8e0d42
Leonardo <Leonardo@DESKTOP-496MDC1>
2022-02-23 23:13:25 -0300

    Adicionar arquivos de projeto.

---
+++ Exception/Entities/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+using NewException.Entities.Exception;
+
+namespace NewException.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            if (withdrawLimit <= 0)
+            {
+                throw new DomainException("Error creating account: The withdraw limit less than zero!");
+            }
+
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            if(amount <= 0)
+            {
+                throw new DomainException("Deposit error: The amount less than or equal to zero!");
+            }
+
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new DomainException("Withdraw error: The amount less than or equal to zero!");
+            }
+
+            if(amount > WithdrawLimit)
+            {
+                throw new DomainException("Withdraw error: The amount exceeds withdraw limit!");
+            }
+
+            if (amount > Balance)
+            {
+                throw new DomainException("Withdraw error: Not enough balance!");
+            }
+
+            Balance -= amount;
+        }
+
+        public string NewBalance()
+        {
+            return "New balance: $ " +
+                    Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exception/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using NewException.Entities;
+using NewException.Entities.Exception;
+
+namespace NewException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, balance, withdrawLimit);
+
+                Console.Write("Enter amount for deposit: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                acc.Deposit(amount);
+                Console.WriteLine(acc.NewBalance());
+
+                Console.Write("Enter amount for withdraw: ");
+                amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                acc.Withdraw(amount);
+                Console.WriteLine(acc.NewBalance());
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
